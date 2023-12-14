ee1b3d178b8741638d62971f56c445c9a10b9a28
Nirajan Dhakal <101518890+dhakalnirajan@users.noreply.github.com>
2023-12-14 21:41:32 +0545

    Update App.js

--- workshop-react/demo/src/App.js
+++ workshop-react/demo/src/App.js
@@ -1,76 +1,76 @@
 import './App.css';
 
-// import Days from './days/Days';
-// function App (){
-//   const days = [
-//     'Sunday',
-//     'Monday',
-//     'Tuesday',
-//     'Wednesday',
-//     'Thursday',
-//     'Friday',
-//     'Saturday',
-//   ];
-//   return (
-//     <>
-//       <Days />
-//       <Days days = {days} />
-//     </>
-//   );
-// }
+import Days from './days/Days';
+function App (){
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return (
+    <>
+      <Days />
+      <Days days = {days} />
+    </>
+  );
+}
 
-// export default App;
+export default App;
 
-// import Display from './display/Display';
+import Display from './display/Display';
 
-// function App () {
-//   return (
-//     <>
-//       <Display name = {"Michael"} age={2000000}/>
-//       <Display name = {"Samael"} age={21000000}/>
-//     </>
-//   );
-// }
+function App () {
+  return (
+    <>
+      <Display name = {"Michael"} age={2000000}/>
+      <Display name = {"Samael"} age={21000000}/>
+    </>
+  );
+}
 
-// export default App;
+export default App;
 
-// import './App.css';
-// import Card from './card/card';
-// import Clock from './clock/Clock';
-// import Todo from './todo/Todo';
-// import {Read} from "./todo/Todo"
+import './App.css';
+import Card from './card/card';
+import Clock from './clock/Clock';
+import Todo from './todo/Todo';
+import {Read} from "./todo/Todo"
 
-// function App () {
-//   const cards = ['Pokhara', 'Kathmandu', 'Butwal'];
+function App () {
+  const cards = ['Pokhara', 'Kathmandu', 'Butwal'];
 
-//   return (
-//     <div className="App">
-//       <>
-//         <Clock />
-//         <Todo />
-//         <Read />
-//       </>
-//       {cards.map (item => {
-//         return <Card item={item} />;
-//       })}
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      <>
+        <Clock />
+        <Todo />
+        <Read />
+      </>
+      {cards.map (item => {
+        return <Card item={item} />;
+      })}
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
-// import Counter from "./counter/Counter";
-// import Hook from "./hook/Hook"
+import Counter from "./counter/Counter";
+import Hook from "./hook/Hook"
 
-// function App(){
-//   return (
-//     <>
-//       <Counter />
-//     </>
-//   );
-// }
+function App(){
+  return (
+    <>
+      <Counter />
+    </>
+  );
+}
 
-// export default App;
+export default App;
 
 import React from 'react';
 import Todo from './todo/Todo';
